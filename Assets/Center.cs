64451bd98113f4d75a6191b8aec290b26c8034c0
Chopin <m.chopin@student.isartdigital.com>
2020-02-26 15:55:17 +0100

    Terrain

---
+++ Assets/Center.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Center : MonoBehaviour
+{
+    public float Speed;
+    public Light Light;
+    public Material Day;
+    public Material Night;
+
+    private void Start()
+    {
+        RenderSettings.skybox = Day;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Speed, 0, 0);
+        Debug.Log(transform.rotation.x);
+        if ((transform.rotation.x > 0.70 || transform.rotation.x < -0.70) && Light.intensity > 0)
+        {
+            Light.intensity -= 0.01f;
+            RenderSettings.skybox = Night;
+        }
+            
+        else if ((transform.rotation.x < 0.70 && transform.rotation.x > -0.70) && Light.intensity < 1)
+        {
+            Light.intensity += 0.01f;
+            RenderSettings.skybox = Day;
+        }
+            
+    }
+}
---
+++ Assets/FirstCamera.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class FirstCamera : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    private const RotationAxes Axes = RotationAxes.MouseXAndY;
+    public float MaximumY = 60F;
+    public float MinimumY = -60F;
+    private float _rotationY;
+    public float SensitivityX = 50F;
+    public float SensitivityY = 50F;
+
+    private void Update()
+    {
+        switch (Axes)
+        {
+            case RotationAxes.MouseXAndY:
+            {
+                var rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * SensitivityX;
+
+                _rotationY += Input.GetAxis("Mouse Y") * SensitivityY;
+                _rotationY = Mathf.Clamp(_rotationY, MinimumY, MaximumY);
+
+                transform.localEulerAngles = new Vector3(-_rotationY, rotationX, 0);
+                break;
+            }
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        Vector3 now;
+        now.y = now.x = 0;
+        now.z = 1;
+        if (Input.GetKey("up"))
+            transform.Translate(now * Time.deltaTime);
+        if (Input.GetKey("down"))
+            transform.Translate(-now * Time.deltaTime);
+        if (Input.GetKey("right"))
+            transform.Translate(Vector3.right * Time.deltaTime);
+        if (Input.GetKey("left"))
+            transform.Translate(-Vector3.right * Time.deltaTime);
+    }
+}
