0078a8ff2a90a091b4e9019c6f5381fd6798cd89
Azatsu <paralyastion@gmail.com>
2020-02-27 09:31:14 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/UI/MenusManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenusManager : MonoBehaviour
+{
+    private int nbPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ControllerToCharactersLobby()
+    {
+        GameObject UImanager = GameObject.FindGameObjectWithTag("UIManager");
+        for (int i =0; i < UImanager.transform.childCount; i++)
+        {
+            if (UImanager.transform.GetChild(i).tag == "CharactersLobby")
+                UImanager.transform.GetChild(i).gameObject.SetActive(true);
+        }
+        GameObject.FindGameObjectWithTag("ControllerLobby").SetActive(false);
+        nbPlayer = GameObject.FindGameObjectWithTag("ControllerLobby").GetComponent<UIPlayerIconManager>().getNbPlayers();
+    }
+
+    public void CharactersToControllerLobby()
+    {
+        GameObject UImanager = GameObject.FindGameObjectWithTag("UIManager");
+        for (int i = 0; i < UImanager.transform.childCount; i++)
+        {
+            if (UImanager.transform.GetChild(i).tag == "ControllerLobby")
+                UImanager.transform.GetChild(i).gameObject.SetActive(true);
+        }
+        GameObject.FindGameObjectWithTag("CharactersLobby").SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/UI/UIPlayerIconManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIPlayerIconManager : MonoBehaviour
+{
+    private int nbPlayer;
+    GameObject[] controllerIcons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerIcons = GameObject.FindGameObjectsWithTag("ControllerIcon");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+            nbPlayer++;
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+            nbPlayer--;
+        int idxIcon = 0;
+        while (idxIcon < 4)
+        {
+            if (idxIcon < nbPlayer)
+            {
+                controllerIcons[idxIcon].GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f);
+            }
+            else
+                controllerIcons[idxIcon].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f);
+            idxIcon++;
+        }
+
+    }
+
+    public int getNbPlayers()
+    {
+        return nbPlayer;
+    }
+}
